a47225d880b795f1cd23485079ded7eddaecd666
ivankolev <ivankolev@gmail.com>
2015-12-09 00:19:18 -0500

    footballscores app general cleanup, take 2

--- PhaseshifterFootballScores/footballscores.app/src/main/java/barqsoft/footballscores/Utilities.java
+++ PhaseshifterFootballScores/footballscores.app/src/main/java/barqsoft/footballscores/Utilities.java
@@ -1,63 +1,86 @@
 package barqsoft.footballscores;
 
+import android.content.Context;
+import android.content.res.Resources;
+
 /**
  * Created by yehya khaled on 3/3/2015.
  */
 public class Utilities
 {
-    public static final int SERIE_A = 357;
-    public static final int PREMIER_LEGAUE = 354;
-    public static final int CHAMPIONS_LEAGUE = 362;
-    public static final int PRIMERA_DIVISION = 358;
-    public static final int BUNDESLIGA = 351;
-    public static String getLeague(int league_num)
+    public static final int CHAMPIONS_LEAGUE = 405;
+    public static final int BUNDESLIGA1 = 394;
+    public static final int BUNDESLIGA2 = 395;
+    public static final int LIGUE1 = 396;
+    public static final int LIGUE2 = 397;
+    public static final int PREMIER_LEAGUE = 398;
+    public static final int PRIMERA_DIVISION = 399;
+    public static final int SEGUNDA_DIVISION = 400;
+    public static final int SERIE_A = 401;
+    public static final int PRIMEIRA_LIGA = 402;
+    public static final int BUNDESLIGA3 = 403;
+    public static final int EREDIVISIE = 404;
+    private Resources res;
+
+    public Utilities(Context context) {
+        this.res  = context.getResources();
+    }
+
+    public String getLeague(int league_num)
     {
         switch (league_num)
         {
-            case SERIE_A : return "Seria A";
-            case PREMIER_LEGAUE : return "Premier League";
-            case CHAMPIONS_LEAGUE : return "UEFA Champions League";
-            case PRIMERA_DIVISION : return "Primera Division";
-            case BUNDESLIGA : return "Bundesliga";
-            default: return "Not known League Please report";
+            case SERIE_A : return res.getString(R.string.group_serie_a);
+            case PREMIER_LEAGUE : return res.getString(R.string.group_premier_league);
+            case CHAMPIONS_LEAGUE : return res.getString(R.string.group_champions_league);
+            case PRIMERA_DIVISION : return res.getString(R.string.group_primera_division);
+            case BUNDESLIGA1 : return res.getString(R.string.group_bundesliga);
+            case BUNDESLIGA2 : return res.getString(R.string.group_bundesliga2);
+            case LIGUE1 : return res.getString(R.string.group_ligue1);
+            case LIGUE2 : return res.getString(R.string.group_ligue2);
+            case SEGUNDA_DIVISION : return res.getString(R.string.group_segunda_division);
+            case PRIMEIRA_LIGA : return res.getString(R.string.group_primeira_liga);
+            case BUNDESLIGA3 : return res.getString(R.string.group_bundesliga3);
+            case EREDIVISIE : return res.getString(R.string.group_eredivisie);
+            default: return res.getString(R.string.group_unknown);
         }
     }
-    public static String getMatchDay(int match_day,int league_num)
+    public String getMatchDay(int match_day,int league_num)
     {
         if(league_num == CHAMPIONS_LEAGUE)
         {
             if (match_day <= 6)
             {
-                return "Group Stages, Matchday : 6";
+                return res.getString(R.string.champs_group_stage);
             }
             else if(match_day == 7 || match_day == 8)
             {
-                return "First Knockout round";
+                return res.getString(R.string.champs_first_round);
             }
             else if(match_day == 9 || match_day == 10)
             {
-                return "QuarterFinal";
+                return res.getString(R.string.champs_quarter_final);
             }
             else if(match_day == 11 || match_day == 12)
             {
-                return "SemiFinal";
+                return res.getString(R.string.champs_semi_final);
             }
             else
             {
-                return "Final";
+                return res.getString(R.string.champs_final);
             }
         }
         else
         {
-            return "Matchday : " + String.valueOf(match_day);
+            return res.getString(R.string.matchday) + String.valueOf(match_day);
         }
     }
 
-    public static String getScores(int home_goals,int awaygoals)
+    public String getScores(int home_goals,int awaygoals)
     {
         if(home_goals < 0 || awaygoals < 0)
         {
-            return " - ";
+            return res.getString(R.string.dash);
         }
         else
         {
--- PhaseshifterFootballScores/footballscores.app/src/main/java/barqsoft/footballscores/scoresAdapter.java
+++ PhaseshifterFootballScores/footballscores.app/src/main/java/barqsoft/footballscores/scoresAdapter.java
@@ -26,9 +26,12 @@ public class scoresAdapter extends CursorAdapter
     public static final int COL_MATCHTIME = 2;
     public double detail_match_id = 0;
     private String FOOTBALL_SCORES_HASHTAG = "#Football_Scores";
+    private Utilities utils;
+
     public scoresAdapter(Context context,Cursor cursor,int flags)
     {
         super(context,cursor,flags);
+        this.utils = new Utilities(context);
     }
 
     @Override
@@ -48,7 +51,7 @@ public class scoresAdapter extends CursorAdapter
         mHolder.home_name.setText(cursor.getString(COL_HOME));
         mHolder.away_name.setText(cursor.getString(COL_AWAY));
         mHolder.date.setText(cursor.getString(COL_MATCHTIME));
-        mHolder.score.setText(Utilities.getScores(cursor.getInt(COL_HOME_GOALS),cursor.getInt(COL_AWAY_GOALS)));
+        mHolder.score.setText(utils.getScores(cursor.getInt(COL_HOME_GOALS),cursor.getInt(COL_AWAY_GOALS)));
         mHolder.match_id = cursor.getDouble(COL_ID);
         mHolder.home_crest.setImageResource(Utilities.getTeamCrestByTeamName(
                 cursor.getString(COL_HOME)));
@@ -68,10 +71,10 @@ public class scoresAdapter extends CursorAdapter
             container.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT
                     , ViewGroup.LayoutParams.MATCH_PARENT));
             TextView match_day = (TextView) v.findViewById(R.id.matchday_textview);
-            match_day.setText(Utilities.getMatchDay(cursor.getInt(COL_MATCHDAY),
+            match_day.setText(utils.getMatchDay(cursor.getInt(COL_MATCHDAY),
                     cursor.getInt(COL_LEAGUE)));
             TextView league = (TextView) v.findViewById(R.id.league_textview);
-            league.setText(Utilities.getLeague(cursor.getInt(COL_LEAGUE)));
+            league.setText(utils.getLeague(cursor.getInt(COL_LEAGUE)));
             Button share_button = (Button) v.findViewById(R.id.share_button);
             //TODO fix, currently crashes
             share_button.setOnClickListener(new View.OnClickListener() {
